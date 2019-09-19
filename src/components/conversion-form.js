d12b0c69ef926d0f68217cbd6c794cea54b704e3
Ryan Hoffmann <ryan.hoffmann@airwis.com>
2019-09-19 13:41:32 -0500

    styling

---
+++ src/components/conversion-form.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+
+import SyncIcon from '@material-ui/icons/SyncAlt';
+
+import currencies from '../utils/currencies';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginBottom: theme.spacing(6),
+  },
+  icon: {
+    margin: theme.spacing(3),
+  },
+}));
+
+const ConversionForm = () => {
+  const classes = useStyles();
+
+  const [currency, setCurrency] = useState(currencies.usd);
+
+  const [arxValue, setArxValue] = useState(0);
+  const [shouldEvaluateArx, setShouldEvaluateArx] = useState(true);
+
+  const [currencyValue, setCurrencyValue] = useState(0);
+  const [shouldEvaluateCurrency, setShouldEvaluateCurrency] = useState(true);
+
+  useEffect(() => {
+    if (shouldEvaluateCurrency) {
+      setCurrencyValue(arxValue / 1250);
+    }
+  }, [arxValue, shouldEvaluateCurrency]);
+
+  useEffect(() => {
+    if (shouldEvaluateArx) {
+      setArxValue(currencyValue * 1250)
+    }
+  }, [currencyValue, shouldEvaluateArx]);
+
+  const handleArxChange = (value) => {
+    setShouldEvaluateArx(false);
+    setShouldEvaluateCurrency(true);
+
+    setArxValue(value);
+  };
+
+  const handleCurrencyChange = (value) => {
+    setShouldEvaluateArx(true);
+    setShouldEvaluateCurrency(false);
+
+    setCurrencyValue(value);
+  };
+
+  return (
+    <Grid item className={classes.container}>
+      <Grid
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+      >
+        <TextField
+          label="ARX"
+          value={arxValue}
+          onChange={e => handleArxChange(e.target.value)}
+        />
+        <SyncIcon className={classes.icon} />
+        <TextField
+          label={currency.name}
+          value={currencyValue}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">{currency.label}</InputAdornment>,
+          }}
+          onChange={e => handleCurrencyChange(e.target.value)}
+        />
+      </Grid>
+    </Grid>
+  )
+};
+
+export default ConversionForm;
--- src/components/root.js
+++ src/components/root.js
@@ -1,73 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField'
 
-import SyncIcon from '@material-ui/icons/SyncAlt';
-
-import currencies from '../utils/currencies';
-import useDebounce from '../hooks/useDebounce';
-
+import ConversionForm from './conversion-form';
 import ConversionTable from './conversion-table';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    height: '100vh',
+  },
+}));
+
 const Root = () => {
-  const [currency, setCurrency] = useState(currencies.usd);
-
-  const [arxValue, setArxValue] = useState(0);
-  const [shouldEvaluateArx, setShouldEvaluateArx] = useState(true);
-
-  const [currencyValue, setCurrencyValue] = useState(0);
-  const [shouldEvaluateCurrency, setShouldEvaluateCurrency] = useState(true);
-
-  useEffect(() => {
-    if (shouldEvaluateCurrency) {
-      setCurrencyValue(arxValue / 1250);
-    }
-  }, [arxValue, shouldEvaluateCurrency]);
-
-  useEffect(() => {
-    if (shouldEvaluateArx) {
-      setArxValue(currencyValue * 1250)
-    }
-  }, [currencyValue, shouldEvaluateArx]);
-
-  const handleArxChange = (value) => {
-    setShouldEvaluateArx(false);
-    setShouldEvaluateCurrency(true);
-
-    setArxValue(value);
-  };
-
-  const handleCurrencyChange = (value) => {
-    setShouldEvaluateArx(true);
-    setShouldEvaluateCurrency(false);
-
-    setCurrencyValue(value);
-  };
+  const classes = useStyles();
 
   return (
-    <Container maxWidth="md" height="100%">
-      <Grid
-        container
-        justify="center"
-        alignItems="center"
-      >
-        <TextField
-          label="ARX"
-          value={arxValue}
-          onChange={e => handleArxChange(e.target.value)}
-        />
-        <SyncIcon />
-        <TextField
-          label={currency.name}
-          value={currencyValue}
-          onChange={e => handleCurrencyChange(e.target.value)}
-        />
-      </Grid>
-      <ConversionTable />
-    </Container>
-  );
+    <>
+      <CssBaseline />
+      <Container maxWidth="md" className={classes.container}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          className={classes.container}
+        >
+          <ConversionForm />
+          <ConversionTable />
+        </Grid>
+      </Container>
+    </>
+  )
 };
 
 export default Root;
