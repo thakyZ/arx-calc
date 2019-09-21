00304ea96aaaca408bd27c60396bd5f80db63585
Ryan Hoffmann <ryan.hoffmann@airwis.com>
2019-09-20 20:57:32 -0500

    a bit of algebra

--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -1,8 +1,31 @@
-export default [
+import _find from 'lodash/find';
+import _findLast from 'lodash/findLast';
+
+import { types as currencyTypes } from './currencies';
+
+const knownValues = [
   { arx: 5000, bonus: 0, gbp: 2.99, eur: 3.49, usd: 3.99 },
   { arx: 8400, bonus: 420, gbp: 4.99, eur: 5.99, usd: 6.99 },
   { arx: 16800, bonus: 900, gbp: 9.59, eur: 11.49, usd: 12.99 },
   { arx: 25500, bonus: 1300, gbp: 12.99, eur: 15.99, usd: 18.99 },
   { arx: 51000, bonus: 3000, gbp: 24.99, eur: 29.99, usd: 37.99 },
   { arx: 85000, bonus: 15000, gbp: 44.99, eur: 54.99, usd: 59.99 },
-]
+];
+
+export const endpoints = (value, isArx, currencyType = currencyTypes.usd) => {
+  const a =_findLast(knownValues, (v) => isArx ? v.arx + v.bonus <= value : v[currencyType] <= value)
+    || { arx: 0, bonus: 0, gbp: 0, eur: 0, usd: 0 };
+
+  const b = _find(knownValues, (v) => isArx ? v.arx + v.bonus >= value : v[currencyType] >= value)
+    || { arx: 170000, bonus: 30000, gbp: 89.98, eur: 109.98, usd: 119.98 };
+
+  return [
+    { x: a.arx + a.bonus, y: a[currencyType] },
+    { x: b.arx + b.bonus, y: b[currencyType] },
+  ];
+};
+
+export const slope = ([a, b]) => (b.y - a.y)/(b.x - a.x);
+export const intercept = (m, {x, y}) => y - (m * x);
+
+export default knownValues;
--- src/utils/currencies.js
+++ src/utils/currencies.js
@@ -1,4 +1,7 @@
-import { pow, round } from 'mathjs';
+import { round } from 'mathjs';
+import { endpoints, slope, intercept } from './conversion'
+
+export const types = { usd: 'usd', gbp: 'gbp', eur: 'eur' };
 
 export default {
   usd: {
@@ -16,14 +19,11 @@ export default {
         else return round(1666.94 * x);
       },
       fromArx: (x) => {
-        if (x <= 5000) return round((0.000798 * x + 4.44089e-16), 2);
-        else if (x <= 8820) return round((0.00078534 * x + 0.0632984), 2);
-        else if (x <= 17700) return round((0.000675676 * x + 1.03054), 2);
-        else if (x <= 26800) return round((0.000659341 * x + 1.31967), 2);
-        else if (x <= 54000) return round((0.000698529 * x + 0.269412), 2);
-        else if (x <= 100000) return round((0.000478261 * x + 12.1639), 2);
+        const e = endpoints(x, true, 'usd');
+        const m = slope(e);
+        const b = intercept(m, e[0]);
 
-        return round((0.0005999 * x), 2);
+        return round(m * x + b, 2);
       },
     },
   },
